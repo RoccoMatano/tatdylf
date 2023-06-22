3b66179f2f06454284146be29468092116bdf0cc
Rocco Matano <rocco@matano.de>
2023-06-22 10:06:05 +0200

    reformat build.cmd

--- build_from_vs_cmd.cmd
+++ build_from_vs_cmd.cmd
@@ -1,2 +1,6 @@
 rc /fotatdylf.res src\tatdylf.rc
-cl /O1 /Os /GL /GS- /Isrc src\tatdylf.cpp src\tatdylf_ui.cpp tatdylf.res kernel32.lib ws2_32.lib user32.lib shell32.lib /link /entry:entry_point /subsystem:console /fixed /merge:.rdata=.text
+@set copts=/O1 /Os /GL /GS- /Isrc
+@set lopts=/entry:entry_point /subsystem:console /fixed /merge:.rdata=.text
+@set libs=kernel32.lib ws2_32.lib user32.lib shell32.lib
+@set infiles=src\tatdylf.cpp src\tatdylf_ui.cpp tatdylf.res
+cl %copts% %infiles% %libs% /link %lopts%
