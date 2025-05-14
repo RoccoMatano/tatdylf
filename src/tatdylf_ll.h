cc28418a989d070ece0fdbeca92e30f95f564b48
Rocco Matano <rocco.matano@baslerweb.com>
2025-05-14 12:32:17 +0200

    fix sample tatdylf.ini

--- src/tatdylf_ll.h
+++ src/tatdylf_ll.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2007-2023 Rocco Matano
+// Copyright 2007-2025 Rocco Matano
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
